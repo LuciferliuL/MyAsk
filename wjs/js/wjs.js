70223040a8ceb809a62db70fe010017fa5914585
UMaster <luciferlikeme@163.com>
2017-07-27 17:10:33 +0800

    1.0.0

--- wjs/js/wjs.js
+++ wjs/js/wjs.js
@@ -2,13 +2,22 @@
  * Created by lucif on 2017/6/18.
  */
 $(function(){
-    var item = document.getElementsByTagName('item');
-    $(window).on("resize",function(){
+    var items = $('.carousel-inner .item');
+    //屏幕大小改变
+    $(window).on('resize',function(){
         var width = $(window).width();
-        for(var i = 0;i < item.length;i ++){
-            if (width > 768){
-                item[i].innerHTML = '<img src="image/slide_0'+i+'_2000x410.jpg" alt="...">';
-            }
+        if (width >= 768){
+            $(items).each(function(index,value){
+                var item = $(this);
+                var imgSrc = item.data('bigImg');
+                item.html('<a href="javascript:;" class="pcImg"><img src="'+imgSrc+'"></a>')
+            })
+        }else{
+            $(items).each(function(index,value){
+                var item = $(this);
+                var itemSrc = item.data('smallImg');
+                item.html('<a href="javascript:;" class="mobileImg"><img src="'+itemSrc+'"></a>');
+            })
         }
-    })
+    }).trigger('resize');
 })
